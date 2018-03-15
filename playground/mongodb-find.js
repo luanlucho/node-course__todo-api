ce736c9bac1184c8f67f045d6955fdd3fea264bc
Lucho Andrade <luan95_12@hotmail.com>
2018-03-15 21:25:15 +1000

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// let obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5aaa51ec094d04ac53b8c8b3')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Lucho'}).count().then((count) => {
+        console.log(`Users count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+    // client.close();
+});
